24417ea303d7d62bb97a75c44d9503d6c164ddec
Micaela Mendez <micaela.mr03@gmail.com>
2023-09-27 20:34:57 -0300

    Ciclo lectivo

--- src/ar/edu/unlam/pb2/Parcial01/CicloLectivo.java
+++ src/ar/edu/unlam/pb2/Parcial01/CicloLectivo.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.pb2.Parcial01;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class CicloLectivo {
@@ -10,14 +11,53 @@ public class CicloLectivo {
 	private Date fechaInicioInscripcion;
 	private Date fechaFinalizacionInscripcion;
 	
-	public CicloLectivo(Integer id, Date fechaInicioCicloLectivo, Date fechaFinalizacionCicloLectivo,
-			Date fechaInicioInscripcion, Date fechaFinalizacionInscripcion) {
+	SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-M-yyyy");
+	
+	public CicloLectivo(Integer id, String fechaInicioInscripcion, String fechaFinalizacionInscripcion,String fechaInicioCicloLectivo, String fechaFinalizacionCicloLectivo) throws ParseException {
 		this.id = id;
-		this.fechaInicioCicloLectivo = fechaInicioCicloLectivo;
-		this.fechaFinalizacionCicloLectivo = fechaFinalizacionCicloLectivo;
-		this.fechaInicioInscripcion = fechaInicioInscripcion;
-		this.fechaFinalizacionInscripcion = fechaFinalizacionInscripcion;
+		this.fechaInicioInscripcion = formatoFecha.parse(fechaInicioInscripcion);
+		this.fechaFinalizacionInscripcion = formatoFecha.parse(fechaFinalizacionInscripcion);
+		this.fechaInicioCicloLectivo =  formatoFecha.parse(fechaInicioCicloLectivo);
+		this.fechaFinalizacionCicloLectivo = formatoFecha.parse(fechaFinalizacionCicloLectivo);
 	}
-	
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getFechaInicioCicloLectivo() {
+		return fechaInicioCicloLectivo;
+	}
+
+	public void setFechaInicioCicloLectivo(Date fechaInicioCicloLectivo) {
+		this.fechaInicioCicloLectivo = fechaInicioCicloLectivo;
+	}
+
+	public Date getFechaFinalizacionCicloLectivo() {
+		return fechaFinalizacionCicloLectivo;
+	}
+
+	public void setFechaFinalizacionCicloLectivo(Date fechaFinalizacionCicloLectivo) {
+		this.fechaFinalizacionCicloLectivo = fechaFinalizacionCicloLectivo;
+	}
+
+	public Date getFechaInicioInscripcion() {
+		return fechaInicioInscripcion;
+	}
+
+	public void setFechaInicioInscripcion(Date fechaInicioInscripcion) {
+		this.fechaInicioInscripcion = fechaInicioInscripcion;
+	}
+
+	public Date getFechaFinalizacionInscripcion() {
+		return fechaFinalizacionInscripcion;
+	}
+
+	public void setFechaFinalizacionInscripcion(Date fechaFinalizacionInscripcion) {
+		this.fechaFinalizacionInscripcion = fechaFinalizacionInscripcion;
+	}	
 }
